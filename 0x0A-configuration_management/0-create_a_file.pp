e9d09e84e162b3e485c587c830fb44253d305fdf
bouzrbay <atighmarcelline@gmail.com>
2023-09-22 10:25:39 -0700

    Done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-#create a file in /tmp.
-
+# manifest to create file in tmp directory
 file { '/tmp/school':
-     ensure => 'present',
-     path   => '/tmp/school',
-     mode   => '0744',
-     owner  => 'www-data',
-     group  => 'www-data',
-     content=> 'I love Puppet',
-     }
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
+}
