65f954fa2069431428fc5f9fc6441f149d453141
ghbouzrbay <atighmarcelline@gmail.com>
2023-10-23 14:05:08 -0700

    Ok

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """script that fetches info about a given employee using an api
-and exports it in json format
-"""
+	and exports it in csv format"""
+
 import json
 import requests
 import sys
@@ -11,25 +11,23 @@ base_url = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == "__main__":
 
-    userid = sys.argv[1]
-    userurl = '{}/users?id={}'.format(base_url, userid)
-    response = requests.get(userurl)
+    user_id = sys.argv[1]
+    user_url = '{}/users?id={}'.format(base_url, user_id)
+    response = requests.get(user_url)
     data = response.text
     data = json.loads(data)
-    username = data[0].get('username')
-    tasksurl = '{}/todos?userId={}'.format(baseurl, userid)
-    response = requests.get(tasksurl)
+    user_name = data[0].get('username')
+    tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
+    response = requests.get(tasks_url)
     tasks = response.text
     tasks = json.loads(tasks)
-    dictionarykey = str(userid)
-    builder = {dictionarykey: []}
+    builder = ""
     for task in tasks:
-        f_jsondata = {
-            "task": task['title'],
-            "completed": task['completed'],
-            "username": username
-        }
-        builder[dictionarykey].append(f_jsondata)
-    encoded_jsondata = json.dumps(builder)
-    with open('{}.json'.format(userid), 'w', encoding='UTF8') as myFile:
-        myFile.write(encoded_jsondata)
+        builder += '"{}","{}","{}","{}"\n'.format(
+            user_id,
+            user_name,
+            task['completed'],
+            task['title']
+        )
+    with open('{}.csv'.format(user_id), 'w', encoding='UTF8') as myFile:
+        myFile.write(builder)
