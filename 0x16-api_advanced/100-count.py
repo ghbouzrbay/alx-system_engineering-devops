96c07b90dd62995dcee155b937e9b8d61c5487e4
root <atighmarcelline@gmail.com>
2023-11-07 19:10:37 +0000

    Done

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,55 +1,53 @@
 #!/usr/bin/python3
 """Function to count words in all hot posts of a given Reddit subreddit."""
+
 import requests
+import re
+import json
+import BeautifulSoup
 
-
-def count_words(subreddit, word_list, instances={}, after="", count=0):
+def count_words(subreddit, word_list,
+    url="https://www.reddit.com/r/{}/hot/",
+    start=None, counter=None):
     """
     Prints counts of given words found in hot posts of a given subreddit.
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
+    if not start:
+        start = 0
+    if not counter:
+        counter = {}
+    for word in word_list:
+        word = word.lower()
+        counter[word] = counter.get(word, 0)
+
+    response = requests.get(url.format(subreddit), headers={"User-agent": "100-main"})
+    if response.status_code != 200:
+        print("Failed to query Reddit API.")
         return
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
-
-    if after is None:
-        if len(instances) == 0:
-            print("")
+    soup = BeautifulSoup(response.text, "html.parser")
+    links = soup.find_all("a", class_="SQnoC3ObvgnGjWt90zD_" if start == 0 else "")
+    for link in links:
+        if "?count=" in link["href"]:
             return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
-    else:
-        count_words(subreddit, word_list, instances, after, count)
+        title = link.get_text()
+        if not title:
+            continue
+        title = re.sub(r"[\W_]+", " ", title)
+        title = title.lower()
+        title = re.sub(r"\b({})\b".format("|".join(word_list)), r"\1 ", title)
+        title = re.sub(r"\s+", " ", title)
+        for word in word_list:
+            counter[word] += title.count(word)
+
+    next_link = soup.find("a", rel="next")
+    if next_link:
+        next_url = "https://www.reddit.com" + next_link["href"]
+        return count_words(subreddit, word_list,
+			   url=next_url, start=start+25,
+     			   counter=counter)
+
+    results = [(k, v) for k, v in counter.items() if v > 0]
+    results.sort(key=lambda x: (-x[1], x[0]))
+    for keyword, count in results:
+        print("{}: {}".format(keyword, count))
